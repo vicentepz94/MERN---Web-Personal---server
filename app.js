fc04167543ae4a67ace602c7320aebec367abc60
vicentepz94 <vicentepz94@gmail.com>
2022-11-05 18:53:39 -0300

    33 y 34, se agregan notas de funcionalidades

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { API_VERSION } = require("./constants");
+
+const app = express();
+
+// Import routing
+// ...
+
+// Configure Body Parse
+// ...
+
+// Configure Header HTTP - CORS
+// ...
+
+//Configure routings
+// ...
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+// Para conectarse a mongoose
 const mongoose = require("mongoose");
+
+// Para levantar el servidor http
+const app = require("./app");
+
+// Para conectarse a mongoose
 const {
   DB_USER,
   DB_PASSWORD,
@@ -7,11 +13,20 @@ const {
   API_VERSION,
 } = require("./constants");
 
+// Para levantar el servidor http
+const PORT = process.env.POST || 3977;
+
+// Para conectarse a mongoose
 mongoose.connect(
   `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`,
   (error) => {
     if (error) throw error;
 
-    console.log("La conexion con la base de datos ha sido exitosa");
+    app.listen(PORT, () => {
+      console.log("######################");
+      console.log("###### API REST ######");
+      console.log("######################");
+      console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}/`);
+    });
   }
 );
