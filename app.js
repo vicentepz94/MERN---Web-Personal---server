9ab564d1583036e546baec75066b1b81f8698e67
vicentepz94 <vicentepz94@gmail.com>
2022-11-06 12:38:47 -0300

    Se agrega dependencia body-parser y se importa en app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const { API_VERSION } = require("./constants");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // Import routing
 // ...
 
-// Configure Body Parse
-// ...
+// Configure Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Configure Header HTTP - CORS
 // ...
