f07a1fea993426f9a4bef718ac2509c78e33cedc
vicentepz94 <vicentepz94@gmail.com>
2022-11-06 16:23:25 -0300

    (40 y 41) se crea el modelo de usuario (con encriptacion de contraseñas, y registro de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,32 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
 function register(req, res) {
-  console.log("Se ha ejecutado el registro");
-  res.status(200).send({ msg: "TODO OK" });
+  const { firstname, lastname, email, password } = req.body;
+
+  if (!email) res.status(400).send({ msg: "El email es obligatorio" });
+  if (!password) res.status(400).send({ msg: "La contraseña es obligatoria" });
+
+  const user = new User({
+    firstname,
+    lastname,
+    email: email.toLowerCase(),
+    role: "user",
+    active: false,
+  });
+
+  // Para encriptar la contraseña con la dependecia de bcryptjs
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  user.password = hashPassword;
+
+  user.save((error, userStorage) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear usuario" });
+    } else {
+      res.status(200).send(userStorage);
+    }
+  });
 }
 
 module.exports = {
