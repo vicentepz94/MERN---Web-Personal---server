c81b7bc1cd047566eddd9fa6be3a83e904781540
vicentepz94 <vicentepz94@gmail.com>
2022-11-06 16:33:41 -0300

    41 se importa dependencia jwt y se crea el access y refresh token para el manejo de sesiones

--- constants.js
+++ constants.js
@@ -5,10 +5,13 @@ const DB_HOST = "web-personal.tk8espn.mongodb.net";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JWT_SECRET_KEY = "wfO@XU%4v52o%7usnEh6mhy";
+
 module.exports = {
   DB_USER,
   DB_PASSWORD,
   DB_HOST,
   API_VERSION,
   IP_SERVER,
+  JWT_SECRET_KEY,
 };
