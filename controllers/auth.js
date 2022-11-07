0d78e2396de536da5da568ea6edfa6f69016e86b
vicentepz94 <vicentepz94@gmail.com>
2022-11-07 01:22:41 -0300

    41 y 42 a la mitad

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("../utils/jwt");
+const e = require("cors");
 
 function register(req, res) {
   const { firstname, lastname, email, password } = req.body;
@@ -29,6 +31,52 @@ function register(req, res) {
   });
 }
 
+function login(req, res) {
+  const { email, password } = req.body;
+
+  if (!email) res.status(400).send({ msg: "El email es obligatorio" });
+  if (!password) res.status(400).send({ msg: "La contraseña es obligatoria" });
+
+  const emailLowerCase = email.toLowerCase();
+
+  User.findOne({ email: emailLowerCase }, (error, userStore) => {
+    if (error) {
+      res.status(400).send({ msg: "Error del servidor" });
+    } else {
+      bcrypt.compare(password, userStore.password, (bcryptError, check) => {
+        if (bcryptError) {
+          res.status(500).send({ msg: "Error del servidor" });
+        } else if (!check) {
+          res.status(400).send({ msg: "Contraseña incorrecta" });
+        } else if (!userStore.active) {
+          res.status(401).send({ msg: "Usuario no autorizado" });
+        }
+        res.status(200).send({
+          access: jwt.createAccessToken(userStore),
+          refresh: jwt.createRefreshToken(userStore),
+        });
+      });
+    }
+  });
+}
+
+function refreshAccessToken(req, res) {
+  const { token } = req.body;
+
+  const { user_id } = jwt.decoded(token);
+
+  User.findOne({ _id: user_id }, (error, userStorage) => {
+    if (error) {
+      res.status(500).send({ msg: "Error del servidor" });
+    } else {
+      res.status(200).send({
+        accessToken: jwt.createAccessToken(userStorage),
+      });
+    }
+  });
+}
+
 module.exports = {
   register,
+  login,
 };
--- router/auth.js
+++ router/auth.js
@@ -5,5 +5,6 @@ const api = express.Router();
 
 // Solo se coloca la ruta en Insomnia para probar los Endpoints
 api.post("/auth/register", AuthController.register);
+api.post("/auth/login", AuthController.login);
 
 module.exports = api;
