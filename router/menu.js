94b41c3220efee1dad7176f35594ba5c013785d7
vicentepz94 <vicentepz94@gmail.com>
2022-11-12 19:49:50 -0300

    Se agrega la estructura del api de menus (en api.js, Controllers/menu.js y router/menu.js

---
+++ router/menu.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const MenuCoontroller = require("../controllers/menu");
+const md_auth = require("../middlewares/authenticated");
+
+const api = express.Router();
+
+// ENDPOINT
+// ...
+
+module.exports = api;
