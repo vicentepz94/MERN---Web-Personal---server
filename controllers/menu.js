86d507d7744d93ab389eed0f8da21ae3859b3103
vicentepz94 <vicentepz94@gmail.com>
2022-11-12 20:13:12 -0300

    54 Creacion de menus en la base de datos

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,3 +1,17 @@
 const Menu = require("../models/menu");
 
-module.exports = {};
+async function createMenu(req, res) {
+  const menu = new Menu(req.body);
+
+  menu.save((error, menuStored) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear menu" });
+    } else {
+      res.status(200).send(menuStored);
+    }
+  });
+}
+
+module.exports = {
+  createMenu,
+};
